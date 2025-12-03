5b431079d4cb9707cdebf9160d536f9dcf4a24f8
Lucas Farias <lucasfnf@gmail.com>
2025-12-02 21:09:17 -0300

    Read inputs to string

--- benches/day1.rs
+++ benches/day1.rs
@@ -1,16 +1,12 @@
 use criterion::{Criterion, criterion_group, criterion_main};
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let data = std::fs::read("inputs/day1.txt").unwrap();
+    let data = std::fs::read_to_string("inputs/day1.txt").unwrap();
     c.bench_function("day1_part1", |b| {
-        b.iter(|| aoc2025::day1::LockSolver::find_password(&String::from_utf8_lossy(&data)))
+        b.iter(|| aoc2025::day1::LockSolver::find_password(&data))
     });
     c.bench_function("day1_part2", |b| {
-        b.iter(|| {
-            aoc2025::day1::LockSolver::find_password_method_0x434C49434B(&String::from_utf8_lossy(
-                &data,
-            ))
-        })
+        b.iter(|| aoc2025::day1::LockSolver::find_password_method_0x434C49434B(&data))
     });
 }
 
--- benches/day2.rs
+++ benches/day2.rs
@@ -1,12 +1,12 @@
 use criterion::{Criterion, criterion_group, criterion_main};
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let data = std::fs::read("inputs/day2.txt").unwrap();
+    let data = std::fs::read_to_string("inputs/day2.txt").unwrap();
     c.bench_function("day2_part1", |b| {
-        b.iter(|| aoc2025::day2::RangeChecker::check_ranges(&String::from_utf8_lossy(&data)))
+        b.iter(|| aoc2025::day2::RangeChecker::check_ranges(&data))
     });
     c.bench_function("day2_part2", |b| {
-        b.iter(|| aoc2025::day2::RangeChecker::check_ranges_extra(&String::from_utf8_lossy(&data)))
+        b.iter(|| aoc2025::day2::RangeChecker::check_ranges_extra(&data))
     });
 }
 
